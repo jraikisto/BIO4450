9b4aa3f5e27e7839faa845ce53078df4638478b6
Jaakko Raikisto <jaakko.raikisto@uta.fi>
2019-03-08 00:51:59 +0200

    Added command for running bowtie

--- ex1.sh
+++ ex1.sh
@@ -1,3 +1,9 @@
 cd
 mkdir ex1
 fastqc /student_data/BIO4450/data/ex1/tumor -o ./
+mkdir bowtie
+cd bowtie
+bowtie2-build /data/references/hg38_chr17.fa ./chr17
+mkdir index
+mv *.bt2 index
+bowtie2 -x index/chr17 -1 /student_data/BIO4450/data/ex1/tumor_1.fa.gz -2 /student_data/BIO4450/data/ex1/tumor_2.fa.gz -S tumor.sam -X 1000 -f
