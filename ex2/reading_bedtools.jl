acb692d92e845f5271525a706e6586b410f9da15
Jaakko Raikisto <jaakko.raikisto@uta.fi>
2019-03-26 00:00:47 +0200

    folder looks sensible now

--- ex2/reading_bedtools.jl
+++ ex2/reading_bedtools.jl
@@ -1,6 +1,7 @@
-
+using DelimitedFiles
 function countAR()
     f = "bedtools_out.bed"
+    tsv = readdlm("abundance.tsv")
     lines = open(f)
 
     reads = zeros(0)
@@ -22,8 +23,25 @@ function countAR()
         end
         if name == "AR"
             push!(reads, parse(Int64, parts[end]))
+
+            #Finding corresponding area in Kallisto
+            print(parts[1])
+            print(" ")
+            id = ""
+            for y in fields
+                s = split(y, " ")
+                if s[2] == "transcript_id"
+                    id = s[3][2:end-1]
+                    break
+                end
+            end
+            estimate = tsv[tsv[:, 1] .== id, 4]
+            isempty(id) ? println() : println("$(id) $(estimate) $(parts[end])")
+
         end
     end
+
+    #Printing AR stats
     println("Overlapping happens in $(length(reads)) transcripts when observing AR")
     println()
     println("Read amounts:")
