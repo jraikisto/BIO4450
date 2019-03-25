26fd066b6a81b0da27e739e21ba310f798698a28
Jaakko Raikisto <jaakko.raikisto@uta.fi>
2019-03-25 21:13:01 +0200

    Exercise 2

---
+++ ex2.sh
@@ -0,0 +1,12 @@
+PATH=$PATH:/opt/binf/apps/hisat2-2.1.0:/opt/binf/apps/samtools-1.9/bin:/opt/binf/apps/bedtools-2.27.1/bin
+
+
+mkdir hisat2
+mkdir hisat2/index
+
+hisat2-build /data/references/hg38.fa hisat2/index/hg38
+
+hisat2 -x ./hisat2/index/hg38 -1 /student_data/BIO4450/data/ex2/patient_1.fq.gz -2 /student_data/BIO4450/data/ex2/patient_2.fq.gz | samtools view -bS -> ./bam/tumor.bam
+cd bam
+samtools sort tumor.bam -o ./sorted_tumor.bam
+bedtools multicov -bams bam/sorted_tumor.bam -bed gencode.v29.annotation.gtf.gz > bedtools_out.bed
---
+++ reading_bedtools.jl
@@ -0,0 +1,34 @@
+
+function countAR()
+    f = "bedtools_out.bed"
+    lines = open(f)
+
+    reads = zeros(0)
+    for line in eachline(lines)
+        parts = split(line, "\t")
+        fields = split(parts[9], ";")
+        fields[1] = " " * fields[1]
+
+        name = ""
+        for y in fields
+            s = split(y, " ")
+            if s[2] == "gene_name"
+                name = s[3][2:end-1]
+                break
+            end
+        end
+        if name == "AR"
+            push!(reads, parse(Int64, parts[end]))
+        end
+    end
+    println("Overlapping happens in $(length(reads)) lines when observing AR")
+    println()
+    println("Read amounts:")
+    for (id, i) in enumerate(reads)
+        println("$(id)  $(round(Int, i))")
+    end
+    println()
+    println("Total number of overlapping reads is $(round(Int, sum(reads)))")
+    #println(length(unique(reads)))
+end
+countAR()
