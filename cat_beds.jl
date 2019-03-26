d7065b0e576428b5aea01ad60bd4217f218c68d9
Jaakko Raikisto <jaakko.raikisto@uta.fi>
2019-03-27 00:34:48 +0200

    Added another functionality

--- cat_beds.jl
+++ cat_beds.jl
@@ -19,10 +19,22 @@ function read_bed(path::String)
     return out
 end
 
-only_counts(path) = map(readlines(path)) do y
+only_counts(path::String) = map(readlines(path)) do y
     parse(Int, split(y, '\t')[end])
 end
 
+function dont_parse(path::String)
+    lines = readlines(path)
+    width=length(split(lines[1], '\t'))
+    #lines = open(path)
+
+
+    out = Array{Any, 2}(undef, length(lines), width)
+    for (id, line) in enumerate(lines)
+        out[id, :] .= split(line, '\t')
+    end
+    return out
+end
 #=function only_counts(path)
     lines = readlines(path)
     #lines = open(path)
