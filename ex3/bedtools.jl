d2d5a890fa6d6f297c23508cc6f6ac8d89789aa1
Jaakko Raikisto <jaakko.raikisto@uta.fi>
2019-03-29 02:10:11 +0200

    Moved stuff, yes yes

--- ex3/bedtools.jl
+++ ex3/bedtools.jl
@@ -11,10 +11,9 @@ function read_bed(path::String)
     for (id, line) in enumerate(lines)
         columns = split(line, '\t')
         out[id, :] .= map(columns) do y
-            #TODO: try and catch is horribly slow. Solution with all(isdigit, a) should prolly be preferred, but then there must be chek for 2E19 notation
-            try
+            if all(isdigit(replace(y, "." => "")))
                 return parse(Float64, y)
-            catch
+            else
                 return y
             end
         end
---
+++ ex3/script.r
@@ -0,0 +1,1 @@
+r <- read.table("/student_data/BIO4450/students/jr424258/ex3/output.bed", header = FALSE, sep = "\t", dec = ".")
